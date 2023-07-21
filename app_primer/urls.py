02f5a25371e72f57b7323432c41687f1823a1479
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-07-21 14:06:00 +0500

    добавил ListView

--- app_primer/urls.py
+++ app_primer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, category_posts, post_detail, profile
+# from .views import index, category_posts, post_detail, profile
+from .views import AllPosts, category_posts, post_detail, profile
 
 app_name = 'app_primer'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', AllPosts.as_view(), name='all_posts'),
     path('category/<slug:slug>/', category_posts, name='category'),
     path('<int:post_id>/', post_detail, name='post_detail'),
     path('profile/<str:username>/', profile, name='profile')
--- app_primer/views.py
+++ app_primer/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post, Category, User
 
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all()
-    all_users = User.objects.all()
-    categories = Category.objects.all()
-    context = {
-        'posts': posts,
-        'all_users': all_users,
-        'categories': categories,
-    }
-    return render(request, 'app_primer/index.html', context)
+class AllPosts(ListView):
+    model = Post
+    template_name = 'app_primer/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_users'] = User.objects.all()
+        context['categories'] = Category.objects.all()
+        return context
+
+
+# def index(request):
+#     posts = Post.objects.all()
+#     all_users = User.objects.all()
+#     categories = Category.objects.all()
+#     context = {
+#         'posts': posts,
+#         'all_users': all_users,
+#         'categories': categories,
+#     }
+#     return render(request, 'app_primer/index.html', context)
 
 
 def category_posts(request, slug):

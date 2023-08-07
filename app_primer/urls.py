0cc040ae1516a2b8dd7611a19be8516d0c40f85b
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-08-07 09:31:38 +0500

    добавил задачу 19,1

--- app_primer/urls.py
+++ app_primer/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 # from .views import index, category_posts, post_detail, profile
 
-from .views import AllPosts, CategoryPosts, PostDetail, contacti
+from .views import AllPosts
 
 app_name = 'app_primer'
 
 urlpatterns = [
     path('', AllPosts.as_view(), name='all_posts'),
-    path('contacti/', contacti, name='contacti'),
-    path('category/<slug:slug>/', CategoryPosts.as_view(), name='category'),
-    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
-    # path('profile/<str:username>/', Profile.as_view(), name='profile')
+
 ]
